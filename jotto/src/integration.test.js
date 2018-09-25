dc1d0fa25be271d279687670d7e8c55f6b199701
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-09-25 19:34:41 -0300

    added guessedWords tests

---
+++ jotto/src/integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    it('updates state correctly for unsuccessful guess', () => {});
+
+    it('updates state correctly for successful guess', () => {});
+  });
+
+  describe('guessed words', () => {
+    it('updates state correctly for unsuccessful guess', () => {});
+
+    it('updates state correctly for successful guess', () => {});
+  });
+});
