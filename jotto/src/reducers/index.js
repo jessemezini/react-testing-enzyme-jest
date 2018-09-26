402b8b206f8a6ab60a1d928ce1cccf7e1b0e9943
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-09-25 23:02:57 -0300

    added thunk for guessedWords state

--- jotto/src/reducers/index.js
+++ jotto/src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 
 import success from './successReducer';
 import guessedWords from './guessedWordsReducer';
+import secretWord from './secretWordReducer';
 
-export default combineReducers({ success, guessedWords });
+export default combineReducers({ success, guessedWords, secretWord });
