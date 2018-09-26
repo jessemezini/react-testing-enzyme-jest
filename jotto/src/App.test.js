fe60e27146cc81d6755d8aeac63d243f55368718
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-09-26 18:06:23 -0300

    connect app comp. and tests

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+import { storeFactory } from '../test/testUtils';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = (state = {}) => {
+  const store = storeFactory(state);
+  const wrapper = shallow(<App store={store} />).dive();
+
+  return wrapper;
+};
+
+describe('redux properties', () => {
+  it('has access to "success" state', () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+
+    expect(successProp).toBe(success);
+  });
+
+  it('has access to "secretWord" state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const successProp = wrapper.instance().props.secretWord;
+
+    expect(successProp).toBe(secretWord);
+  });
+
+  it('has access to "guessedWords" state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const successProp = wrapper.instance().props.guessedWords;
+
+    expect(successProp).toBe(guessedWords);
+  });
+
+  it('"getSecretWord" action creator is a func. on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
